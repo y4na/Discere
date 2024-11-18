efd0205ee754633d9f4420c25f127f19ada1827d
Yana <paragosoyana@gmail.com>
2024-11-18 20:24:34 +0800

    updated profile pop up

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -40,6 +40,7 @@ module.exports = {
          * and make sure the pattern below matches your project structure.
          */
         // '../../**/*.py'
+        "./node_modules/flowbite/**/*.js",
     ],
     theme: {
         extend: {
@@ -70,5 +71,6 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
+        require('flowbite/plugin'),
     ],
 }
