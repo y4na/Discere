de3c92bbb763236cd9b94f0c0b39575f43eb61b6
Capstone <brix.bitayo@gmail.com>
2024-12-09 15:17:18 +0800

    Added forgot password for now

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
-
+    path('password-reset/', views.send_verification_code, name='password_reset'),
+    path('verify-code/', views.verify_code, name='verify_code'),
+    path('password-reset/', views.send_verification_code, name='send_verification_code'),
+    path('reset-password/<str:email>/', views.update_password, name='update_password'),  # Pass email via URL
 ]
